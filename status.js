5fc292bfb0f255fcfb2454fa2e2cfd906383b969
sidosan <filipe.sidosan@gmail.com>
2020-10-13 21:04:58 +0000

    end logger

--- status.js
+++ status.js
@@ -1,6 +1,6 @@
 
 const os = require('os')
-
+const log = require('./logger')
 
 setInterval(() => {
     const { freemem, totalmem } = os
@@ -18,5 +18,7 @@ setInterval(() => {
     console.clear()
     console.table(" ====== PC STATS ======")
     console.table(stats)
+    
+    log(`${JSON.stringify(stats)}\n`)
 
 }, 1000)
